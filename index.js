6dcc3c923e9b8b069125e048f7c0c3a82c07ef58
Jean Felipe <JFBimCopinski@gmail.com>
2024-06-12 19:30:49 -0300

    Init: added the entire code

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 console.log("Você toca algum instrumento?")
 
 process.stdin.on("data", function(data){
-    entrada = data.toString().trim()
+    entrada = data.toString().trim().toLowerCase()
     if (entrada == "sim" || entrada == "s") {
-        console.log("Você deve tocar bem")
+        console.log("Você deve tocar bem.")
         process.exit()
     } else if (entrada == "não" || entrada == "n") {
         console.log("Hmm... que pena!")
         process.exit()
     } else {
         console.log("Digite uma resposta válida (sim / não)")
-    }
+    } 
 })
